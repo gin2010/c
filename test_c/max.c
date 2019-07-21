9a68b10142b69c7108b8dcc128ab1f7af1a901d8
gin2010 <gin2010@users.noreply.github.com>
2019-07-21 19:15:45 +0800

    c

---
+++ test_c/max.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+
+int max(int a ,int b)
+{
+	if (a>b){
+		return a;
+	}else{
+		return b;
+	}
+}
---
+++ test_c/maxnum.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+//#include "max.c"
+
+
+int main(){
+	int a = 10;
+	int b = 11;
+	int maxn = max(a,b);
+	printf("max is %d\n",maxn);
+	return 0 ;
+
+}
